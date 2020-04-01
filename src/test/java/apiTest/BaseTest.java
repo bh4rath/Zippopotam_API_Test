b6263228d09d39dc49ad5ce20cbf989519bed07f
Bharath <bharathgates33@gmail.com>
2020-04-01 10:45:58 +0530

    Updated TestNg.xml file.

--- src/test/java/apiTest/BaseTest.java
+++ src/test/java/apiTest/BaseTest.java
@@ -2,12 +2,12 @@ package apiTest;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
     protected static RequestSpecification requestSpec;
 
-    @BeforeClass
+    @BeforeTest
     public static void createRequestSpecification(){
         requestSpec = new RequestSpecBuilder().setBaseUri("http://api.zippopotam.us")
                 .build();
